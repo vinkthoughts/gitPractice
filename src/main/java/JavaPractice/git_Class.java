8f9fc1d94e414f710ba24685a9302e386173046c
vink <karpevinayak2811@gmail.com>
2025-05-27 20:25:11 +0530

    This is commit from Eclipse project

--- src/main/java/JavaPractice/git_Class.java
+++ src/main/java/JavaPractice/git_Class.java
@@ -5,5 +5,7 @@ public class git_Class {
     public void method(){
 
         System.out.println("this is code from intellij");
+        
+        System.out.println("this is code from eclipse after taking pull from intellij code and also add a new class here name of git_Class2");
     }
 }
---
+++ src/main/java/JavaPractice/git_Class2.java
@@ -0,0 +1,31 @@
+package JavaPractice;
+
+public class git_Class2 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+	public void Method2() {  
+
+		System.out.println("This is method created in eclipse");
+
+
+	}
+
+	public void Method3() {  
+
+		System.out.println("This is method created in eclipse");
+
+
+	}
+
+	public void Method4() {  
+
+		System.out.println("This is method created in eclipse");
+
+
+	}
+
+}
