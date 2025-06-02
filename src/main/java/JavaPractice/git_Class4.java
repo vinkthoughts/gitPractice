475ab012dc94511036374a927c87feb0b4f74c3b
vink <karpevinayak2811@gmail.com>
2025-06-02 17:45:15 +0530

    commit after creating git4 file in intellij in developement branch

---
+++ src/main/java/JavaPractice/git_Class4.java
@@ -0,0 +1,20 @@
+package JavaPractice;
+
+public class git_Class4 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("main method created in class git_Class4");
+
+    }
+
+    public void Method() {
+
+        System.out.println("method created in class git_Class4");
+
+
+    }
+
+
+}
