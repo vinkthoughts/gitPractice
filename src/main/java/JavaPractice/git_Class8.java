ea7ea9df69fc97bb89c11f17c96484483dc3e2f5
vink <karpevinayak2811@gmail.com>
2025-06-03 17:05:18 +0530

    commit in developement branch after creating git class 7 and 8 in eclipse

---
+++ src/main/java/JavaPractice/git_Class8.java
@@ -0,0 +1,19 @@
+package JavaPractice;
+
+public class git_Class8 {
+
+	public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("main method created in class git_Class8");
+
+    }
+
+    public void Method() {
+
+        System.out.println("method created in class git_Class8");
+
+
+    }
+	
+}
